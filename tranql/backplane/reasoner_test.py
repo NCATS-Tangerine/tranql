7f7b7724a7a5bfde6aea1ca2e8f963e85ac992d2
frostyfan109 <urnancx@gmail.com>
2019-06-26 16:03:41 -0400

    Improved readability and functionality of reasoner_test.py

--- tranql/backplane/reasoner_test.py
+++ tranql/backplane/reasoner_test.py
@@ -1,70 +1,90 @@
 import requests
 import json as JSON
 
-# url = "https://indigo.ncats.io/reasoner/api/v1/query"
-url = "https://rtx.ncats.io/beta/api/rtx/v1/query"
+# This file is for testing reasoner APIs. It directly queries the selected API, not the backplane.
+# (Not a unit test)
 
+INDIGO = "https://indigo.ncats.io/reasoner/api/v1/query"
+RTX = "https://rtx.ncats.io/beta/api/rtx/v1/query"
 data1 = {
-  "edges": [
-    {
-      "edge_id": "e00",
-      "source_id": "n00",
-      "target_id": "n01",
-      "type": "targets"
+  "type":INDIGO,
+  "query": {
+      "edges": [
+        {
+          "edge_id": "e00",
+          "source_id": "n00",
+          "target_id": "n01",
+          "type": "targets"
+        }
+      ],
+      "nodes": [
+        {
+          "node_id": "n00",
+          "curie": "CHEMBL:CHEMBL521",
+          "type": "chemical_substance",
+        },
+        {
+          "node_id": "n01",
+          "type": "protein"
+        }
+      ]
+  }
+}
+data2 = {
+  "type":RTX,
+  "query": {
+      "edges": [
+        {
+          "edge_id": "e1",
+          "source_id": "chemical_substance",
+          "target_id": "protein"
+        }
+      ],
+      "nodes": [
+        {
+          "node_id": "chemical_substance",
+          "type": "chemical_substance",
+          "curie":"CHEMBL.COMPOUND:CHEMBL25"
+        },
+        {
+          "node_id": "protein",
+          "type": "protein"
+        }
+      ]
+  }
+}
+data3 = {
+    "type":RTX,
+    "query": {
+        "edges": [
+          {
+            "edge_id": "e1",
+            "source_id": "metabolite",
+            "target_id": "protein"
+          }
+        ],
+        "nodes": [
+          {
+            "node_id": "metabolite",
+            "type": "metabolite",
+            "curie":"KEGG:C00017"
+          },
+          {
+            "node_id": "protein",
+            "type": "protein"
+          }
+        ]
     }
-  ],
-  "nodes": [
-    {
-      "node_id": "n00",
-      "curie": "CHEMBL:CHEMBL521",
-      "type": "chemical_substance",
-    },
-    {
-      "node_id": "n01",
-      "type": "protein"
-    }
-  ]
 }
 
-data2 = {
-  "edges": [
-    {
-      "edge_id": "e1",
-      "source_id": "chemical_substance",
-      "target_id": "protein"
+active_query = data3
+
+if active_query["type"] == INDIGO or active_query["type"] == RTX:
+    json = {
+        "query_message": {
+            "query_graph": active_query["query"]
+        }
     }
-  ],
-  "nodes": [
-    {
-      "node_id": "chemical_substance",
-      "type": "chemical_substance",
-      "curie":"CHEMBL.COMPOUND:CHEMBL25"
-    },
-    {
-      "node_id": "protein",
-      "type": "protein"
-    }
-  ]
-}
-# {
-#   "query_message": {
-#     "original_question": "What proteins are the target of acetaminophen",
-#     "query_type_id": "Q3",
-#     "restated_question": "What proteins are the target of acetaminophen",
-#     "terms": {
-#       "chemical_substance": "CHEMBL.COMPOUND:CHEMBL112",
-#       "rel_type": "physically_interacts_with",
-#       "target_label": "protein"
-#     }
-#   },
-#   "bypass_cache": "false",
-#   "max_results": 5
-# }
-json = {
-    "query_message": {
-        "query_graph": data2
-    }
-}
-r = requests.post(url,json=json)
+r = requests.post(active_query["type"],json=json)
 
 print(r,r.text)
