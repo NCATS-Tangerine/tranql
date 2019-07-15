68807fa45d71671ec2bd366a8aeb2fa520f80ddf
Steven Cox <scox@renci.org>
2019-07-15 11:06:57 -0400

    include regex from query, condidtionally. doesnt work w/wf5.

--- tranql/backplane/server.py
+++ tranql/backplane/server.py
@@ -290,7 +290,7 @@ class ICEESClusterQuery(StandardAPIResource):
                 result[k] = val[1]
                 
         """ Filter ids returned by ICEES to ones we can currently make use of. """
-        result["regex"] = "(MONDO|HP):.*"
+        #result["regex"] = "(MONDO|HP):.*"
             
         return result
 
