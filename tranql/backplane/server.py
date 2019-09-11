56b9d19df5e3da73e19e932b56c385dcc0ada202
Steven Cox <scox@renci.org>
2019-09-11 14:29:14 -0400

    increase max results from gamma

--- tranql/backplane/server.py
+++ tranql/backplane/server.py
@@ -652,7 +652,8 @@ class GammaResource(StandardAPIResource):
         super().__init__()
         self.robokop_url = 'https://robokop.renci.org' # TODO - make a configuration setting.
         self.view_post_url = f'{self.robokop_url}/api/simple/view/'
-        self.quick_url = f'{self.robokop_url}/api/simple/quick/?rebuild=false&output_format=MESSAGE&max_connectivity=0&max_results=25'
+        self.quick_url = f'{self.robokop_url}/api/simple/quick/?rebuild=false&output_format=MESSAGE&max_connectivity=0&max_results=300'
+        #                                                      ?rebuild=false&output_format=MESSAGE&max_connectivity=0&max_results=250
     def view_url (self, uid):
         return f'{self.robokop_url}/simple/view/{uid}'
 
