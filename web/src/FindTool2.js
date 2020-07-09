c8f48258d81b04e07a9ab7754d3b6838d0017606
frostyfan109 <urnancx@gmail.com>
2020-07-09 12:36:21 -0400

    Lots of changes to find tool

--- web/src/FindTool2.js
+++ web/src/FindTool2.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import elasticlunr from 'elasticlunr';
 import { debounce } from './Util.js';
 import './FindTool2.css';
@@ -19,48 +20,86 @@ export default class FindTool2 extends Component {
   }
   _results() {
     const input = this.state.search;
+    // If there's no input don't display the results and waste time building an entire search index
+    if (input === "") return null;
     const searchIndex = elasticlunr(function() {
       // Nodes
       this.addField("name");
       this.addField("equivalent_identifiers");
 
       // Edges
+      this.addField("source_id");
+      this.addField("target_id");
       this.addField("predicate_id");
       this.addField("description");
 
       // Neutral
       this.addField("id");
-      // Type field breaks elasticlunr in schema view for whatever reason
       this.addField("type");
 
-      this.setRef("id");
+      // This property should uniquely identify a result among nodes and edges - cannot be an array/object
+      this.setRef("index_identifier");
     });
-    this.props.graph.nodes.forEach((node) => searchIndex.addDoc(node.origin));
-    this.props.graph.links.forEach((link) => searchIndex.addDoc(link.origin));
+    let identifier = 0;
+    const identifierIndex = {}; // store nodes/edges for each index_identifier
+
+    const NODE = 0;
+    const LINK = 1;
+    function addToDoc(nodeOrLink, type) {
+      // Get current identifier and post-inc identifier
+      const currentId = identifier++;
+      identifierIndex[currentId] = {type: type, element: nodeOrLink.origin};
+      searchIndex.addDoc({
+        index_identifier: currentId,
+        ...nodeOrLink.origin
+      });
+    }
+    this.props.graph.nodes.forEach((node) => addToDoc(node, NODE));
+    this.props.graph.links.forEach((link) => addToDoc(link, LINK));
     const results = searchIndex.search(input, {
       expand: true
     });
+    window.elastic = elasticlunr;
     // console.log(results);
     // const results = this.props.graph.nodes.filter((node) => node.id.includes(input));
-    return (
-      <div>
-        <div className="find-tool-result find-tool-result-header">{results.length} results</div>
-        {
-          results.map((match, i) => {
-            return (
-              <div className="find-tool-result" key={i}>
-                {
-                  match.ref
-                }
-              </div>
-            );
-          })
-        }
-      </div>
-    );
+    function Results() {
+      const resultsPerPage = 10;
+      const [pages, setPages] = useState(1);
+      const getNumResults = () => Math.min(results.length, pages * resultsPerPage);
+      return (
+        <div className="find-tool-result-container">
+          <div className="find-tool-result find-tool-result-header">Showing {getNumResults()} of {results.length} results</div>
+          {
+            results.slice(0, getNumResults()).map((match, i) => {
+              return (
+                <div className="find-tool-result" key={i}>
+                  {
+                    (function() {
+                      const { type, element } = identifierIndex[match.ref];
+                      if (type === NODE) {
+                        return element.hasOwnProperty("name") ? `${element.name} (${element.id})` : element.id;
+                      } else {
+                        const type = Array.isArray(element.type) ? element.type : [element.type];
+                        return element.source_id + "-[" + type.join(", ") + "]->" + element.target_id;
+                      }
+                    })()
+                  }
+                </div>
+              );
+            })
+          }
+          {
+            getNumResults() < results.length && (<div className="w-100 d-flex justify-content-center align-items-center">
+              <Button variant="link" onClick={() => setPages(pages + 1)}>Show more</Button>
+            </div>)
+          }
+        </div>
+      );
+    }
+    return <Results/>
   }
   _setInput() {
-    this.setState({ search : this._input.current.textContent });
+    this.state.active && this.setState({ search : this._input.current.value });
   }
   show() {
     this.setState({ active : true }, () => {
@@ -78,10 +117,8 @@ export default class FindTool2 extends Component {
 
     return (
       <div className="FindTool">
-        <span contentEditable={true} className="find-tool-input" autoFocus placeholder="Search:" ref={this._input} onInput={this._setInput}/>
-        <div className="find-tool-result-container">
-          {this._results()}
-        </div>
+        <input className="find-tool-input" autoFocus placeholder="Search:" ref={this._input} type="text" onInput={this._setInput}/>
+        {this._results()}
       </div>
     );
   }
